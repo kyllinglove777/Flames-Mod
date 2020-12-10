0d213a0b793ba15e033ab42778721f2aa32e83b3
Jørgen Tharaldsen Fosstvedt <kyllinglove@gmail.com>
2020-12-10 21:40:16 +0100

    uploaded some vanity. Including some scuffed leggings

---
+++ Items/ArmorAndVanity/Vanity/SkiJacket.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fmod.Items.ArmorAndVanity.Vanity
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class SkiJacket : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ski jacket");
+            Tooltip.SetDefault("Warms your body while skiing");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = ItemRarityID.LightRed;
+            item.vanity = true;
+        }
+    }
+}
---
+++ Items/ArmorAndVanity/Vanity/SkiLeggings.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fmod.Items.ArmorAndVanity.Vanity
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class SkiLeggings : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ski leggings");
+            Tooltip.SetDefault("Warms your body while skiing");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = ItemRarityID.LightRed;
+            item.vanity = true;
+        }
+    }
+}
