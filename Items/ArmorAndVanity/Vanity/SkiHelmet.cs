ebd5e93368280a036e931b5f56adb50cf3c32ef9
Jørgen Tharaldsen Fosstvedt <kyllinglove@gmail.com>
2020-12-14 07:53:52 +0100

    The entire ski vanity set now works!

---
+++ Items/ArmorAndVanity/Vanity/SkiHelmet.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fmod.Items.ArmorAndVanity.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class SkiHelmet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ski helmet");
+            Tooltip.SetDefault("Warms your head while skiing");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = ItemRarityID.LightRed;
+            item.vanity = true;
+        }
+        public override bool DrawHead()
+        {
+            return true;
+        }
+    }
+}
